dfa15115abad20c5c71a537535a51bcbab62b15e
Luke Terry <luke.terry0682@gmail.com>
2023-02-14 11:03:10 -0600

    problem 2

--- assignment3.js
+++ assignment3.js
@@ -6,3 +6,11 @@ function sumOfList(list){
     list.forEach((i) => sum += i)
     return sum
 }
+
+function productOfList(list){
+    // returns the product of the given array
+    // sending non-numerical values will cause wonky behavior
+    product = 1
+    list.forEach((i) => product *= i)
+    return product
+}
