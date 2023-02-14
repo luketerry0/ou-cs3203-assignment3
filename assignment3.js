70a6463eb6617f439074cfe9ec5aa1a0bbed75e0
Luke Terry <luke.terry0682@gmail.com>
2023-02-14 11:14:55 -0600

    add 'main' function, node library dependencies

--- assignment3.js
+++ assignment3.js
@@ -1,3 +1,32 @@
+// use a prompt library since node's runtime is different than google's V8 runtime which runs in chrome
+const prompt = require('prompt-sync')();
+
+// "main" function since there is no direct equivelant in javascript
+// this immediate function will execute automatically and guide the user through the script
+(function(){
+    console.log("Hello! Please input a number then press enter. To Stop adding numbers, input * \n");
+    num = prompt("");
+    list = [];
+
+    while (num != "*"){
+        num = parseInt(num)
+        if (!isNaN(num)){
+            list.push(num)
+        }else{
+            console.log("Previous input is not a number (and wasn't added to the list!) Type a number:")
+        }
+        num = prompt("")
+    }
+
+    console.log("Sum of Numbers: ")
+    console.log(sumOfList(list) + " \n")
+
+
+    console.log("Product of Numbers: ")
+    console.log(productOfList(list) + " \n")
+
+}())
+
 
 function sumOfList(list){
     // returns the sum of the given array
